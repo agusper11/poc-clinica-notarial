71ecf6d80e8e5ce33ba940116a194b22e7197c02
agusper11 <agustin.perez@estudiantes.utec.edu.uy>
2025-09-19 23:24:45 +0000

    arreglo a app.py

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def status():
     return jsonify({'status': 'OK', 'message': 'Backend de prueba funcionando'})
 
+@app.route('/')
+def index():
+    return "API Clinica Notarial PoC is running!"
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
 
